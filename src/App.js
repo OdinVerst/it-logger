512a73865bf95ba574dbada30ad9e7f0774ba8b4
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-08-08 17:40:21 +0300

    Add SearchNavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, {useEffect} from 'react';
+import React, { Fragment, useEffect } from 'react';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchNavbar from './components/layout/SearchNavbar';
 
-const App = ()=> {
-  useEffect(()=> {
+const App = () => {
+  useEffect(() => {
     //Init matarialize JS
     M.AutoInit();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchNavbar />
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/SearchNavbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchNavbar = () => {
+  return (
+    <nav className="blue" style={{ marginBottom: '1em;' }}>
+      <div className="nav-wrapper">
+        <form>
+          <div className="input-field">
+            <input id="search" type="search" />
+            <label className="label-icon" htmlFor="search">
+              <i className="material-icons">search</i>
+            </label>
+            <i className="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchNavbar;
